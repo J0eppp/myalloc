e357650107b201485f4989b53ea83036a6e892b6
Joep van Dijk <joep.van.dijk04@gmail.com>
2025-07-13 23:28:02 +0200

    Simple testing for myalloc library

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include "myalloc/myalloc.h"
 
-int main(int, char**){
-    printf("Hello, from myalloc!\n");
+int main(int, char **)
+{
+    void *ptr = alloc(32);
+    printf("Pointer address: %p\n", ptr);
+
+    printf("Printing heap\n");
+    print_stack();
+    printf("Done printing heap\n");
+
+    void *ptr2 = alloc(8);
+    printf("Pointer address: %p\n", ptr2);
+
+    printf("Printing heap\n");
+    print_stack();
+    printf("Done printing heap\n");
+
+
+    printf("Deallocating pointers\n");
+    dealloc(ptr);
+    ptr = NULL;
+    dealloc(ptr2);
+    ptr2 = NULL;
+
+    printf("Printing heap\n");
+    print_stack();
+    printf("Done printing heap\n");
 }
